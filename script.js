315377a31f3180b851df2bf94057f6c71f9b81cd
Sagi Gleizer <sagigleizer@gmail.com>
2018-03-28 18:12:41 +0300

    font and margin changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 window.onload = function() {
     document.getElementById('add').addEventListener("click", addListItem);
+    document.getElementById('sort').addEventListener("click", sortList);
 }
 
 function addListItem(){
@@ -11,3 +12,24 @@ function addListItem(){
     document.getElementById('list').appendChild(node);
 }
 
+function sortList() {
+    let  list, i, switching, b, shouldSwitch;
+    list = document.getElementById('list');
+    switching = true;
+    // switching loop till all switch needed to be done are done
+    while (switching){
+        switching = false;
+        b = list.getElementsByTagName("LI");
+        for (i = 0; i < (b.length -1); i++){
+            shouldSwitch = false;
+            if (b[i].innerHTML.toLocaleLowerCase() > b[i + 1].innerHTML.toLowerCase()){
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch){
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+        }
+    }
+}
